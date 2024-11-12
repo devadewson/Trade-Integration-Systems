4451f16eddfcb14f42dfe6d6057b864feaad9c88
Agung Baskara Pambudi <ABPambudi@maybank.co.id>
2024-11-12 18:11:30 +0700

    Fixing Dynamic Loop to Create new Instance

--- src/main/java/com/maybank/integratorapp/service/NewDynamicJmsListenerService.java
+++ src/main/java/com/maybank/integratorapp/service/NewDynamicJmsListenerService.java
@@ -47,16 +47,18 @@ public class NewDynamicJmsListenerService {
 
     public void configureListeners(List<MsQueueConfig> queueConfigs) {
         for (MsQueueConfig config : queueConfigs) {
-
-            if ((!config.getRequest_Queue_Name().equals(null)
-                    && !config.getRequest_Queue_Name().equals(""))
-            ) {
-                if (config.getEnableStatus() == 1) {
-                    createAndRegisterNewListener(config);
-                } else {
-                    stopExistingListener(config);
+            if(!sessions.containsKey(config.getServiceName())){
+                if ((!config.getRequest_Queue_Name().equals(null)
+                        && !config.getRequest_Queue_Name().equals(""))
+                ) {
+                    if (config.getEnableStatus() == 1) {
+                        createAndRegisterNewListener(config);
+                    } else {
+                        stopExistingListener(config);
+                    }
                 }
             }
+
         }
     }
 
@@ -74,8 +76,8 @@ public class NewDynamicJmsListenerService {
         Session session = null;
         try {
             //Development Use Only
-//            if (!config.getServiceName().equals("CustomerDetails"))
-//                return;
+            if (!config.getServiceName().equals("SwiftOut"))
+                return;
 
             // Create a new connection
             ConnectionFactory connectionFactory = createIBMConnectionFactory(
