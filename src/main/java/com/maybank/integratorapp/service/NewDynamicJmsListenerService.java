9d5566a180f5c263097049d2692227f83d606df5
Syafiqur Rahman <syafiqurrahman30@gmail.com>
2024-11-13 09:52:46 +0700

    Cherry :
    	- Fixing Dynamic Loop to Create new Instance

--- src/main/java/com/maybank/integratorapp/service/NewDynamicJmsListenerService.java
+++ src/main/java/com/maybank/integratorapp/service/NewDynamicJmsListenerService.java
@@ -51,18 +51,19 @@ public class NewDynamicJmsListenerService {
 
     public void configureListeners(List<MsQueueConfig> queueConfigs) {
         for (MsQueueConfig config : queueConfigs) {
-            if(!sessions.containsKey(config.getServiceName())){
-                if ((!config.getRequest_Queue_Name().equals(null)
-                        && !config.getRequest_Queue_Name().equals(""))
-                ) {
-                    if (config.getEnableStatus() == 1) {
-                        createAndRegisterNewListener(config);
-                    } else {
-                        stopExistingListener(config);
+            if(!sessions.containsKey(config.getServiceName())) {
+                if (!sessions.containsKey(config.getServiceName())) {
+                    if ((!config.getRequest_Queue_Name().equals(null)
+                            && !config.getRequest_Queue_Name().equals(""))
+                    ) {
+                        if (config.getEnableStatus() == 1) {
+                            createAndRegisterNewListener(config);
+                        } else {
+                            stopExistingListener(config);
+                        }
                     }
                 }
             }
-
         }
     }
 
